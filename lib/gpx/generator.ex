f6142ee4a4b7232f23856c0dc063af46a18f5041
Brian Berlin <bri@nberl.in>
2018-12-28 22:35:22 -0600

    cleanup

--- lib/gpx/generator.ex
+++ lib/gpx/generator.ex
@@ -15,14 +15,14 @@ defmodule Gpx.Generator do
     json
     |> Enum.with_index(1)
     |> Enum.map(&item/1)
-    |> Enum.join("\n")
+    |> Enum.join("")
   end
 
   def item({[lat, lon], index}) do
     """
-    <wpt lat="#{lat}" lon="#{lon}">
-      <time>#{Timex.shift(Timex.now(), seconds: index * 1)}</time>
-    </wpt>
+      <wpt lat="#{lat}" lon="#{lon}">
+        <time>#{Timex.shift(Timex.now(), seconds: index * 1)}</time>
+      </wpt>
     """
   end
 
@@ -30,8 +30,7 @@ defmodule Gpx.Generator do
     """
     <?xml version="1.0"?>
     <gpx version="1.1" creator="gpxgenerator.com">
-    #{content}
-    </gpx>
+      #{content}</gpx>
     """
   end
 end
